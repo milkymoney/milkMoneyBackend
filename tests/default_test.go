f566c3ba69bdd158c366881993c4eabe361855ad
wtysos11 <wtysos11@163.com>
2019-06-04 20:01:47 +0800

    成功拿取数据

--- tests/default_test.go
+++ tests/default_test.go
@@ -6,8 +6,10 @@ import (
 	"testing"
 	"runtime"
 	"path/filepath"
+	"encoding/json"
+	"io/ioutil"
 	_ "github.com/milkymoney/milkMoneyBackend/routers"
-
+	"github.com/milkymoney/milkMoneyBackend/models"
 	"github.com/astaxie/beego"
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/astaxie/beego/logs"
@@ -24,15 +26,16 @@ func TestGet(t *testing.T) {
 	r, _ := http.NewRequest("GET", "/v1/task/?userId=2", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
-
+	var tasks []models.Task
+	readBuf,_ := ioutil.ReadAll(w.Body)
+	json.Unmarshal(readBuf, &tasks)
 	logs.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
-
+	logs.Trace(tasks)
+	logs.Trace(tasks[0].Id)
+	logs.Trace(tasks[0].Userid)
 	Convey("Subject: Test Station Endpoint\n", t, func() {
 	        Convey("Status Code Should Be 200", func() {
 	                So(w.Code, ShouldEqual, 200)
-	        })
-	        Convey("The Result Should Not Be Empty", func() {
-	                So(w.Body.Len(), ShouldBeGreaterThan, 0)
 	        })
 	})
 }
