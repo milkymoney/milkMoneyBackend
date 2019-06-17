8f89ec8d0d371527e07c09f2034f4a139cc0c9f7
erosWu <31613446+erosWu@users.noreply.github.com>
2019-06-17 23:15:38 +0800

    Update default_test.go
    添加了各个GET方法API的单元测试

--- tests/default_test.go
+++ tests/default_test.go
@@ -9,6 +9,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	_ "github.com/milkymoney/milkMoneyBackend/routers"
+	"github.com/milkymoney/milkMoneyBackend/controllers"
 	"github.com/milkymoney/milkMoneyBackend/models"
 	"github.com/astaxie/beego"
 	. "github.com/smartystreets/goconvey/convey"
@@ -21,7 +22,7 @@ func init() {
 	beego.TestBeegoInit(apppath)
 }
 
-// TestGet is a sample to run an endpoint test
+TestGet is a sample to run an endpoint test
 func TestGetAllTask(t *testing.T) {
 	r, _ := http.NewRequest("GET", "/v1/task/?userId=2", nil)
 	w := httptest.NewRecorder()
@@ -32,10 +33,10 @@ func TestGetAllTask(t *testing.T) {
 	json.Unmarshal(readBuf, &tasks)
 	logs.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
 	logs.Trace(tasks)
-	/*
+	
 	logs.Trace(tasks[0].Id)
 	logs.Trace(tasks[0].Userid)
-*/
+	
 	Convey("Subject: Test Station Endpoint\n", t, func() {
 	        Convey("Status Code Should Be 200", func() {
 	                So(w.Code, ShouldEqual, 200)
@@ -43,3 +44,151 @@ func TestGetAllTask(t *testing.T) {
 	})
 }
 
+func TestOwnTask(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/v1/task/publisher?userId=2", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	var tasks []models.Task
+	readBuf,_ := ioutil.ReadAll(w.Body)
+	json.Unmarshal(readBuf, &tasks)
+	logs.Trace(tasks)
+	// logs.Trace(tasks[1].Type)
+	// logs.Trace(tasks[1].Description)
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+	        Convey("Status Code Should Be 200", func() {
+	                So(w.Code, ShouldEqual, 200)
+	        })
+	})
+}
+
+func TestOwnTaskId(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/v1/task/publisher/2?userId=2", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	var tasks models.Task
+	readBuf,_ := ioutil.ReadAll(w.Body)
+	json.Unmarshal(readBuf, &tasks)
+	logs.Trace(tasks.Id)
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+			Convey("Status Code Should Be 200", func() {
+				So(w.Code, ShouldEqual, 200)
+			})
+			Convey("Task Id should Be 2", func() {
+				So(tasks.Id, ShouldEqual, 2)
+			})
+	})
+}
+
+func TestAllInfomation(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/v1/task/publisher/confirm/2?userId=3", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	var tasks []models.Task
+	readBuf,_ := ioutil.ReadAll(w.Body)
+	json.Unmarshal(readBuf, &tasks)
+	logs.Trace(tasks)
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+	})
+}
+
+func TestTaskRecipient(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/v1/task/recipient/?userId=3", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	var tasks []models.Task
+	readBuf,_ := ioutil.ReadAll(w.Body)
+	json.Unmarshal(readBuf, &tasks)
+	logs.Trace(tasks[0].Id)
+	logs.Trace(tasks[0].Userid)
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("Task Id Should Be 2", func() {
+			So(tasks[0].Id,ShouldEqual, 2)
+		})
+		Convey("UserID Who Release The Task Should Be 2", func() {
+			So(tasks[0].Userid,ShouldEqual, 2)
+		})
+	})
+}
+
+func TestTaskRecipientById(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/v1/task/recipient/2?userId=3", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	var tasks models.Task
+	readBuf,_ := ioutil.ReadAll(w.Body)
+	json.Unmarshal(readBuf, &tasks)
+	logs.Trace(tasks.Id)
+	logs.Trace(tasks.MaxAccept)
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("Task Id Should Be 2", func() {
+			So(tasks.Id,ShouldEqual, 2)
+		})
+		Convey("MaxAccept Should Be 5", func() {
+			So(tasks.MaxAccept,ShouldEqual, 5)
+		})
+	})
+}
+
+
+func TestTaskRecipientFinished(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/v1/task/recipient/settleup/2?userId=3", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	var tasks controllers.AcceptorCheckFinishCodeResponse
+	readBuf,_ := ioutil.ReadAll(w.Body)
+	json.Unmarshal(readBuf, &tasks)
+	logs.Trace(tasks.Proves)
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("Prove URL Should Be fadfas", func() {
+			So(tasks.Proves[0], ShouldEqual, "fadfas")
+		})
+	})
+}
+
+func TestUser(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/v1/task/user?userId=3", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	var tasks models.Task
+	readBuf,_ := ioutil.ReadAll(w.Body)
+	json.Unmarshal(readBuf, &tasks)
+	logs.Trace(tasks.Id)
+	logs.Trace(tasks.MaxAccept)
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("Task Id Should Be 2", func() {
+			So(tasks.Id,ShouldEqual, 2)
+		})
+		Convey("MaxAccept Should Be 5", func() {
+			So(tasks.MaxAccept,ShouldEqual, 5)
+		})
+	})
+}
