3052163b1abe0cd9597b81b600da304b03f55746
wtysos11 <wtysos11@163.com>
2019-06-26 22:22:29 +0800

    change database

--- models/myOrm.go
+++ models/myOrm.go
@@ -9,7 +9,7 @@ import(
 func init() {
 	fmt.Println("Begin to connect to sql")
     orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "root:wu97112500@tcp(129.204.7.185:30000)/demo?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", "root:97112500@tcp(114.115.163.164:30000)/demo?charset=utf8")
 	orm.RegisterModel(new(User),new(Task),new(AcceptRelation),new(ReleaseRelation),new(ConfirmImage))
 	orm.RunSyncdb("default", false, true)
 	test()
